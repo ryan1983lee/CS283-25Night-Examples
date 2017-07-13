aeeec8783efa958662cd679232bce7a0d11dff40
ryan1983lee <ryan1983lee@gmail.com>
2017-07-13 12:44:59 +0800

    第二次上傳

---
+++ Repo/CS283-Homework1/CS283-Homework1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonLibrary
+
+
+namespace CS283_Homework1
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Pokemon apokemon = new Pokemon()
+            {
+                name = "妙蛙種子",
+                hight = "0.67 m",
+                weight = "5.94 kg",
+                gender = "公",
+                category = "草系 ; 毒系",
+                hp = "60",
+                weapon = "籐鞭 & 汙泥炸彈"
+            };
+            Console.WriteLine(
+                "怪物名稱：{0}, 身高{1}, 體重：{2}, 性別：{3}, 類別：{4}, 血量：{5}, 武器：{6}"
+                , Pokemon.name
+                , Pokemon.hight
+                , Pokemon.weight
+                , Pokemon.gender
+                , Pokemon.category
+                , Pokemon.hp
+                , Pokemon.weapon
+                );
+        }
+    }
+}
---
+++ Repo/PokemonLibrary/PokemonLibrary/Pokemon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonLibrary
+{
+    public class Pokemon
+    {
+        public string name;
+        public string hight;
+        public string weight;
+        public string gender;
+        public string category;
+        public string hp;
+        public string weapon;
+    }
+}
