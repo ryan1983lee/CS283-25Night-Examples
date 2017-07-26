99521f5027f662f6b49efe39c19c8b3c5e6a5771
ryan1983lee <ryan1983lee@gmail.com>
2017-07-26 16:32:21 +0800

    HW2

---
+++ Repo/CS283-Homework2/CS283-Homework2/Form1.Designer.cs
@@ -0,0 +1,236 @@
+﻿namespace CS283_Homework2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.InputlistBox = new System.Windows.Forms.ListBox();
+            this.ResultlistBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Enterbutton = new System.Windows.Forms.Button();
+            this.Clearbutton = new System.Windows.Forms.Button();
+            this.Autobutton = new System.Windows.Forms.Button();
+            this.Restartbutton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Source Code Pro", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(30, 40);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(64, 68);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Source Code Pro", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.Red;
+            this.textBox2.Location = new System.Drawing.Point(118, 40);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(64, 68);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Font = new System.Drawing.Font("Source Code Pro", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox3.ForeColor = System.Drawing.Color.Red;
+            this.textBox3.Location = new System.Drawing.Point(206, 40);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(64, 68);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Font = new System.Drawing.Font("Source Code Pro", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox4.ForeColor = System.Drawing.Color.Red;
+            this.textBox4.Location = new System.Drawing.Point(295, 40);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(64, 68);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // InputlistBox
+            // 
+            this.InputlistBox.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.InputlistBox.ForeColor = System.Drawing.Color.Red;
+            this.InputlistBox.FormattingEnabled = true;
+            this.InputlistBox.ItemHeight = 21;
+            this.InputlistBox.Location = new System.Drawing.Point(30, 167);
+            this.InputlistBox.Name = "InputlistBox";
+            this.InputlistBox.Size = new System.Drawing.Size(96, 193);
+            this.InputlistBox.TabIndex = 4;
+            // 
+            // ResultlistBox
+            // 
+            this.ResultlistBox.Font = new System.Drawing.Font("微軟正黑體", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ResultlistBox.FormattingEnabled = true;
+            this.ResultlistBox.ItemHeight = 21;
+            this.ResultlistBox.Location = new System.Drawing.Point(263, 167);
+            this.ResultlistBox.Name = "ResultlistBox";
+            this.ResultlistBox.Size = new System.Drawing.Size(96, 193);
+            this.ResultlistBox.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 136);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Input";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(274, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 24);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Result";
+            // 
+            // Enterbutton
+            // 
+            this.Enterbutton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Enterbutton.Location = new System.Drawing.Point(30, 383);
+            this.Enterbutton.Name = "Enterbutton";
+            this.Enterbutton.Size = new System.Drawing.Size(96, 68);
+            this.Enterbutton.TabIndex = 8;
+            this.Enterbutton.Text = "輸入";
+            this.Enterbutton.UseVisualStyleBackColor = true;
+            // 
+            // Clearbutton
+            // 
+            this.Clearbutton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Clearbutton.Location = new System.Drawing.Point(150, 383);
+            this.Clearbutton.Name = "Clearbutton";
+            this.Clearbutton.Size = new System.Drawing.Size(96, 68);
+            this.Clearbutton.TabIndex = 9;
+            this.Clearbutton.Text = "清除";
+            this.Clearbutton.UseVisualStyleBackColor = true;
+            // 
+            // Autobutton
+            // 
+            this.Autobutton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Autobutton.Location = new System.Drawing.Point(30, 466);
+            this.Autobutton.Name = "Autobutton";
+            this.Autobutton.Size = new System.Drawing.Size(329, 68);
+            this.Autobutton.TabIndex = 10;
+            this.Autobutton.Text = "自動猜測";
+            this.Autobutton.UseVisualStyleBackColor = true;
+            // 
+            // Restartbutton
+            // 
+            this.Restartbutton.Font = new System.Drawing.Font("微軟正黑體", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Restartbutton.Location = new System.Drawing.Point(263, 383);
+            this.Restartbutton.Name = "Restartbutton";
+            this.Restartbutton.Size = new System.Drawing.Size(96, 68);
+            this.Restartbutton.TabIndex = 11;
+            this.Restartbutton.Text = "重玩";
+            this.Restartbutton.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(10, 111);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(373, 24);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "=================================";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Source Code Pro", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(10, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(373, 24);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "=================================";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(388, 544);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Restartbutton);
+            this.Controls.Add(this.Autobutton);
+            this.Controls.Add(this.Clearbutton);
+            this.Controls.Add(this.Enterbutton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ResultlistBox);
+            this.Controls.Add(this.InputlistBox);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "猜數字小程式";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.ListBox InputlistBox;
+        private System.Windows.Forms.ListBox ResultlistBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Enterbutton;
+        private System.Windows.Forms.Button Clearbutton;
+        private System.Windows.Forms.Button Autobutton;
+        private System.Windows.Forms.Button Restartbutton;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
